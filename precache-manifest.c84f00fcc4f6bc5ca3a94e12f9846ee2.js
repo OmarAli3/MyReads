e94c71a7f61cee93b8b73dd231d1c2c1c53e1509
Omar Ali Hassan <36759605+OmarAli3@users.noreply.github.com>
2021-11-23 20:40:35 +0200

    Updates

---
+++ precache-manifest.c84f00fcc4f6bc5ca3a94e12f9846ee2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "209f7e1665adef77d45a",
+    "url": "/MyReads/static/css/main.d16c87aa.chunk.css"
+  },
+  {
+    "revision": "209f7e1665adef77d45a",
+    "url": "/MyReads/static/js/main.209f7e16.chunk.js"
+  },
+  {
+    "revision": "655ad57ae885658cd4ce",
+    "url": "/MyReads/static/js/1.655ad57a.chunk.js"
+  },
+  {
+    "revision": "970c0060ff174c587b9c",
+    "url": "/MyReads/static/js/runtime~main.970c0060.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/MyReads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/MyReads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/MyReads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "167ee7ff63d104c25f6a31bf5a3d3ba4",
+    "url": "/MyReads/index.html"
+  }
+];
